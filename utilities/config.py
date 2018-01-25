192e9516ca20761e490f784c9f51d0aa8bb6ffd7
Zepu Zhang <zepu.zhang@gmail.com>
2018-01-24 23:29:48 -0800

    config

--- utilities/config.py
+++ utilities/config.py
@@ -1,13 +1,4 @@
 """
-The main point of this module is that the location of config files
-is a matter of system design, while child projects should not hard-code
-fixed absolute paths for their config files.
-
-Current design is that environment variable ``${CFGDIR}`` points to
-the directory where config files reside. Below this directory there
-can be sub-directories. Individual projects know the path (including file name)
-of their config files **below** ``${CFGDIR}``.
-
 A commonly used config file format is ``INI``. Here is an example file::
 
     [server]
@@ -52,44 +43,18 @@ For complex values, use JSON strings. Note, quotation marks in JSON strings shou
 """
 
 import configparser
-import os
 
 
-class ConfigFileNotFoundError(Exception):
-    pass
-
-
-def get_config_file(topic):
+def read_ini_config(file_name: str, section_name: str = None, allow_no_value: bool = True) -> dict:
     """
-    Obtain the full path of a config file given its short name.
-
-    Args:
-        topic (str): short name of the config file.
-
-            The environment variable ``CFGDIR`` is guaranteed to exist and point
-            to the directory where config files reside.
-            ``topic`` is the config file's name along with its path under ``${CFGDIR}``,
-            that is, ``topic`` is formatted like ``subdirectory/filename``, hence
-            the full path of the config file is ``${CFGDIR}/topic``.
-
-            If the named file is not found, ``ConfigFileNotFoundError`` is raised.
-    """
-    topic = os.path.join(os.environ['CFGDIR'], topic)
-    if os.path.isfile(topic):
-        return topic
-    raise ConfigFileNotFoundError('Configuration "' + topic + '" can not be found')
-
-
-def read_ini_config(topic, allow_no_value=True):
-    """
-    This is a convenience function for getting the content
-    of an INI-style config file (for read-only).
+    This is a convenience function for getting the content of an INI-style config file
+    (for reading only).
 
     The returned object works much like a ``dict`` thanks to the
     *mapping protocol access* capabilities in Python 3. For example,
-    assuming the config file above is ``${CFGDIR}/myproject/test.cfg``::
+    assuming the config file is '/myproject/test.cfg'::
 
-        conf = read_ini_config('myproject/test.cfg')
+        conf = read_ini_config('/myproject/test.cfg')
         print(conf['server']['host'])   # print '1.2.3.4'
 
         server = conf['server']
@@ -98,6 +63,8 @@ def read_ini_config(topic, allow_no_value=True):
         # Now can pass 'server' on and use it largely as if it's a `dict`.
     """
     conf = configparser.ConfigParser(allow_no_value=allow_no_value)
-    conf.read(get_config_file(topic))
+    conf.read(file_name)
+    if section_name:
+        return conf[section_name]
     return conf
 
