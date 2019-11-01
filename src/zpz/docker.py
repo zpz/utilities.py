37d9ff6899b1dae9da9aa56e2daa592b9f79bf5c
Zepu Zhang <zepu.zhang@gmail.com>
2019-10-31 23:19:05 -0700

    docker utilities

---
+++ src/zpz/docker.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def get_docker_host_ip():
+    # INTERNAL_HOST_IP=$(ip route show default | awk '/default/ {print $3})')
+    # another idea:
+    # ip -4 route list match 0/0 | cut -d' ' -f3
+    #
+    # Usually the result is '172.17.0.1'
+
+    z = subprocess.check_output(['ip', '-4', 'route', 'list', 'match', '0/0'])
+    z = a.decode()[len('default via ') :]
+    return z[: z.find(' ')]
