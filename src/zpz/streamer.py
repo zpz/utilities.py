f2e4d5de00725d920607768b69885041cace08b3
Zepu Zhang <zepu.zhang@gmail.com>
2020-05-04 00:52:55 -0700

    streamer

--- src/zpz/streamer.py
+++ src/zpz/streamer.py
@@ -97,6 +97,9 @@ class Batcher:
 
     
 class Unbatcher:
+    '''
+    An `Unbatcher` object does the opposite of `Batcher`.
+    '''
     def __init__(self, input_stream: Union[AsyncIterable, AsyncIterator]):
         if not isinstance(input_stream, AsyncIterator):
             assert isinstance(input_stream, AsyncIterable)
@@ -256,9 +259,21 @@ class BufferedSink:
             self,
             input_stream: AsyncIterable,
             sink_func: Callable[[Any], Union[asyncio.Task, asyncio.Future]],
+            *,
             buffer_size: int,
             skip_exceptions: bool = False,
     ):
+        '''
+        `sink_func`: takes a single input, returns a `Task`
+        (created by `create_task`) or a `Future` (created by
+        `run_in_executor`); see `Streamer.sink` and `Streamer.mp_sink`.
+        Examples of the real action include: save the element to disk,
+        or compute an aggregation (and save it somewhere).
+
+        This class does NOT keep the order of elements as they are consumed.
+        If order of element consumption needs to be maintained, use
+        `Sink`, not `BufferedSink`.
+        '''
         assert isinstance(input_stream, AsyncIterable)
         self._input_stream = input_stream
         self._sink_func = sink_func
@@ -338,6 +353,83 @@ class Sink:
 
 
 class Streamer:
+    '''
+    Consider an iterable IN, from which we retrieve one element
+    at a time, and an **async** operator P, which takes an element
+    from IN and produces a result (we call it a "transformer").
+    Typically P is I/O bound or otherwise can benefit from concurrency.
+
+    Suppose we can retrieve one element from IN much faster than P processes
+    one element. However, suppose P can benefit from concurrent use
+    (remember P is **async**). This is often the case if P involves I/O such
+    as file saving/loading or RESTful service calls. If we retrieve
+    elements **quickly** from IN, and call P **concurrently**,
+    the output end could produce results quickly.
+
+    `Streamer` is designed for this use case. It takes one input iterable.
+    One or more "transformers" can be added to it. Each element
+    of the input stream goes through the transformers in order and
+    produces an output. For any transformer that can benefit from
+    concurrency, use can specify a "buffer_size" so that elements
+    "pile up" in front of it in order to make full use of concurrency.
+
+    Note, this "piling up" is the main benefit of `Streamer`.
+    If one has multiple operations that are called sequentially without
+    any concurrency, it is better to chain up these operations in a function
+    outside of `Streamer`, and add that function as a single "transformer".
+    It is OK to add the operations one by one as separate "transformers",
+    but there is overhead.
+
+    There are 3 ways to use a `Streamer` object:
+
+    1. Add a "sink" after zero or more "transformers" by calling `sink`,
+        then calling `drain` or `a_drain` on the returned object.
+
+        The `sink` method takes an async function as input, which takes
+        and processes an output element of the last "transformer"
+        (when `transform` has been called before `sink`)
+        or of the "input_stream" (when no "transformer" has been added).
+
+        Note that when "sink" is used, the streamer does **note**
+        have to contain "transformers". It's perfectly fine that the sole
+        purpose is to put the input stream into the sink, e.g.
+        inserting elements of a long list into a DB.
+
+        Often one wants to define a class in order to keep state of the
+        processing, e.g.
+
+            class Receiver:
+                def __init__(self):
+                    self.result = 0
+
+                async def __call__(self, x):
+                    self.result += x
+
+            receiver = Receiver()
+            streamer = Streamer(...)
+            streamer.transform(...).sink(receiver).drain()
+
+            print(receiver.result)
+
+        In `Receiver.__call__`, one can do progress logging
+        and all kinds of other things.
+
+    2. In an async environment, iterate over the output of the last
+        "transformer" and do whatever one desires, e.g.
+
+            streamer = Streamer(...).transform(...).transform(...)
+            async for x in streamer:
+                ... # use `x`
+
+    3. In simple case, call `collect` to gather the output of the
+        last "transformer" into a `list` or anything that defines
+        `append`, e.g.
+
+            streamer = Streamer(...).transform(...).transform(...)
+            output = []
+            streamer.collect(output)
+            print(len(output))
+    '''
     def __init__(
             self,
             input_stream: AsyncIterable,
@@ -349,7 +441,15 @@ class Streamer:
         self._loop = loop or asyncio.get_event_loop()
         self._n_process_workers = mp.cpu_count() + (n_process_workers or 0)
 
+    def __del__(self):
+        if self._process_executor:
+            self._process_executor.shutdown()
+
     def batch(self, batch_size: int, timeout_seconds: float = None):
+        '''
+        Add a batcher is equivalent to providing an `input_stream`
+        that is an iterable of batches.
+        '''
         self._transformers.append(
             Batcher(
                 self._transformers[-1],
@@ -370,6 +470,13 @@ class Streamer:
             keep_order: bool = True,
             return_exceptions: bool = False,
     ):
+        '''
+        This method can be called multiple times to register
+        a chain of transformers in order.
+
+        `keep_order` is ignored if `buffer_size` is `None` or `0`,
+        in which case order is always kept.
+        '''
         if buffer_size and buffer_size > 1:
             self._transformers.append(
                 BufferedTransformer(
@@ -397,6 +504,9 @@ class Streamer:
             buffer_size: int = None,
             **kwargs,
     ):
+        '''
+        Add an *async* transformer and run it in async loop.
+        '''
         if buffer_size and buffer_size > 1:
             def transformer_(x):
                 return self._loop.create_task(trans_func(x, *args))
@@ -406,6 +516,27 @@ class Streamer:
 
         return self._transform(transformer_, buffer_size=buffer_size, **kwargs)
 
+    def mp_transform(self, trans_func: Callable[[Any], Any], *args, **kwargs):
+        '''
+        Add a *sync* transformer and run it in a sequential process.
+        If you want to take advantage of an async function,
+        then wrap it with `asyncio.new_event_loop).run_until_complete`
+        before passing into this method.
+        '''
+        if self._process_executor is None:
+            self._process_executor = ProcessPoolExecutor(self._n_process_workers)
+
+        def transformer_(x):
+            return self._loop.run_in_executor(
+                self._process_executor,
+                trans_func,
+                x,
+                *args,
+            )
+
+        self._transform(transformer_, **kwargs)
+        return self
+
     def _sink(
             self,
             sink_func: Callable[[Any], Union[asyncio.Task, asyncio.Future]],
@@ -413,6 +544,31 @@ class Streamer:
             buffer_size: int = None,
             skip_exceptions: bool = False,
     );
+        '''
+        Add a "sink" at the end of the streamer, after zero or more
+        "transformers".
+
+        A sink is a terminal point of the streamer.
+        Sink is concerned about saving results in files, DB, etc,
+        or computing summaries.
+
+        Since the output, if any, of "sink" is ignored, if "sink"
+        needs to keep any result, it must use some class state
+        or global variable, or persistent storage.
+
+        Returns:
+            A `BufferedSink` object if `buffer_size > 1`;
+            a `Sink` object otherwise.
+
+        Upon getting the output, call its `drain` method if in a sync
+        environment, like
+
+            Streamer(...).transform(...).transform(...).sink(...).drain()
+
+        or `a_drain` method if in an async environment, like
+
+            await Streamer(...).transform(...).transform(...).sink(...).a_drain()
+        '''
         if buffer_size and buffer_size > 1:
             return BufferedSink(
                 self._transformers[-1],
@@ -428,6 +584,11 @@ class Streamer:
              *args,
              buffe_size: int = None,
              **kwargs):
+        '''
+        `sink_func` is an *async* function that takes a single input but
+        does not return anyting that is expected to be consumed.
+        Example: async file writing; DB insertion.
+        '''
         if buffe_size is None or buffe_size < 2:
             async def sink_(x):
                 await sink_func(x, *args)
@@ -437,7 +598,26 @@ class Streamer:
 
         return self._sink(sink_func, buffer_size=buffe_size, **kwargs)
 
+    def mp_sink(self, sink_func: Callable[[Any], None], *args, **kwargs):
+        '''
+        Add a *sync* sink to be run in a separate process.
+        Refer to `mp_transform`.
+        '''
+        if self._process_executor is None:
+            self._process_executor = ProcessPoolExecutor(self._n_process_workers)
+
+        def sink_(x):
+            return self._loop.run_in_executor(
+                self._process_executor, sink_func, x, *args)
+
+        return self._sink(sink_, **kwargs)
+
     def __aiter__(self):
+        '''
+        This is used in async environments to iterate over
+        the output of the last "transformer", and do whatever
+        needed with it.
+        '''
         z = self._transformers[-1]
         if not isinstance(z, AsyncIterator):
             assert isinstance(z, AsyncIterable)
@@ -445,6 +625,15 @@ class Streamer:
         return z
 
     def collect(self, out, log_every: int = 1000) -> None:
+        '''
+        Collect the output of the last "transformer" into a
+        `list` or anything that implements `append`.
+
+        This is a convenience method suitable when the output
+        needs to be simply collected w/o other processing.
+        If sophisticated processing of each output is needed,
+        use `__aiter__` or add a custom "sink".
+        '''
         async def foo():
             n = 0
             async for x in self:
