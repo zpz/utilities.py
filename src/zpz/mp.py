ef445babcfbe124d62fa2c9dab1680363d267c83
Zepu Zhang <zepu.zhang@gmail.com>
2020-12-20 16:22:58 -0800

    add ServerProcess

--- src/zpz/mp.py
+++ src/zpz/mp.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import sys
+from multiprocessing.managers import BaseManager
 from traceback import format_exc
 
 # Using `concurrent.futures.ProcessPoolExecutor`
@@ -10,6 +11,8 @@ from traceback import format_exc
 
 
 MAX_THREADS = min(32, multiprocessing.cpu_count() + 4)
+# This default is suitable for I/O bound operations.
+# For others, user may want to specify a smaller value.
 
 
 def full_class_name(cls):
@@ -21,9 +24,11 @@ def full_class_name(cls):
 
 # TODO
 # check out `https://github.com/ionelmc/python-tblib`
+#
+# about pickling Exceptions with tracebacks.
 
 
-class SubProcessError(Exception):
+class MpError(Exception):
     # An object of this class can be properly pickled
     # to survice transmission in `multiprocessing.Queue`.
     def __init__(self, e: Exception):
@@ -48,8 +53,8 @@ class SubProcessError(Exception):
 
 
 def _my_excepthook(type_, value, tb):
-    if type_ is SubProcessError:
-        # With this hook, the printout upon 
+    if type_ is MpError:
+        # With this hook, the printout upon
         #   `raise SubProcessError(ValueError('wrong value'))`
         # is like what happens upon
         #   `raise ValueError('wrong value')`
@@ -63,3 +68,48 @@ def _my_excepthook(type_, value, tb):
 
 
 sys.excepthook = _my_excepthook
+
+
+class ServerProcess:
+    # User should subclass this class to implement the
+    # functionalities they need.
+    # An instance of this class is created in a "server process"
+    # and serves as shared data for other processes.
+    #
+    # Usage:
+    #
+    #   (1) in main process,
+    #
+    #       obj = ServerProcess.start(...)
+    #
+    #   (2) pass `obj` to other processes
+    #
+    #   (3) in other processes, cal public methods of `obj`.
+    #
+    # `obj` is NOT an instance of the class `ServerProcess`.
+    # It's a "proxy" object, which is like a reference to a
+    # `ServerProcess` object in the "server process".
+    # All public methods of `ServerProcess` can be used on this
+    # proxy object from other processes.
+    # Input and output should all be small, pickle-able
+    # objects.
+    #
+    # When all references to this proxy object have
+    # been garbage-collected, the server process is shut down.
+    # Usually user doesn't need to worry about it.
+    # In order to proactively shut down the server process,
+    # delete (all references to) the proxy object.
+    #
+    # `ServerProcess.start()` can be used multiple times
+    # to have multiple shared objects, which reside in diff
+    # processes and are independent of each other.
+    @classmethod
+    def start(cls, *args, **kwargs):
+        BaseManager.register(
+            cls.__name__,
+            cls,
+        )
+        manager = BaseManager()
+        manager.start()
+        obj = getattr(manager, cls.__name__)(*args, **kwargs)
+        return obj
