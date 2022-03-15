54275730401b76149d293466c96b51ed969477ae
zpz <zepu.zhang@gmail.com>
2022-03-14 21:49:46 -0700

    add module docker

--- src/zpz/docker.py
+++ src/zpz/docker.py
@@ -1,13 +1,15 @@
 import subprocess
 
-def get_docker_host_ip():
+def get_host_ip():
     # INTERNAL_HOST_IP=$(ip route show default | awk '/default/ {print $3})')
     # another idea:
     # ip -4 route list match 0/0 | cut -d' ' -f3
     #
     # Usually the result is '172.17.0.1'
+    # This is the "loopback" address used in code to connect to the host machine.
+    # In most documentation this is said to be '127.0.0.1'.
 
     z = subprocess.check_output(['ip', '-4', 'route', 'list', 'match', '0/0'])
     z = z.decode()[len('default via ') :]
     return z[: z.find(' ')]
-    
+
