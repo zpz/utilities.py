d45a9525ecea40d8c5298dbd55799cbe6886d6fd
zpz <zepu.zhang@gmail.com>
2022-09-17 23:04:10 -0700

    logging

--- src/zpz/logging.py
+++ src/zpz/logging.py
@@ -7,25 +7,22 @@ Usually the 'level' argument is the only argument one needs to customize::
 
   config_logger(level='info')
 
-If `level` is not specified, environment variable `LOGLEVEL` is used;
-if that is not set, a default level (currently 'info') is used.
-
-Do not call this in library modules.
-Library modules should have ::
+Do not call this in library modules. Library modules should have ::
 
    logger = logging.getLogger(__name__)
 
 and then just use ``logger`` to write logs without concern about formatting,
 destination of the log message, etc.
 """
+import inspect
 import logging
-import time
+import logging.handlers
+import os
+import sys
 import warnings
 from datetime import datetime
-from logging import Formatter
 from typing import Union, Dict
 
-import pytz
 
 # When exceptions are raised during logging, then,
 # the default implementation of handleError() in Handler
@@ -42,92 +39,126 @@ import pytz
 #       a message 'No handlers could be found for logger X.Y.Z'
 #       is printed once.
 
-# logging.logThreads = 0
-# logging.logProcesses = 0
+
+# Turn off annoyance in ptpython when setting DEBUG logging
+logging.getLogger('parso').setLevel(logging.ERROR)
+
+logging.captureWarnings(True)
+warnings.filterwarnings('default', category=ResourceWarning)
+warnings.filterwarnings('default', category=DeprecationWarning)
+warnings.filterwarnings(
+    'ignore', category=DeprecationWarning, module='ptpython')
+warnings.filterwarnings(
+    'ignore', category=DeprecationWarning, module='jedi')
 
 
-def log_level_to_str(level):
-    '''
-    `level`: `logging.DEBUG`, `logging.INFO`, etc.
-    '''
-    return logging.getLevelName(level)
-    # Return uppercase 'DEBUG', 'INFO', etc.
+rootlogger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 
-def log_level_from_str(level):
-    '''
-    `level`: 'debug', 'info', etc.
-    '''
-    return getattr(logging, level.upper())
+def formatter(*, with_process_name: bool = False, with_thread_name: bool = False):
+    tz = datetime.now().astimezone().tzname()
+    msg = '[%(asctime)s.%(msecs)03d ' + tz + \
+        '; %(levelname)s; %(name)s, %(funcName)s, %(lineno)d'
 
-
-def _make_config(
-        *,
-        level: Union[str, int] = 'info',
-        with_process_name: bool = False,
-        with_thread_name: bool = False,
-        timezone: str = 'local',
-        **kwargs) -> Dict:
-    # 'level' is string form of the logging levels:
-    #    'debug', 'info', 'warning', 'error', 'critical'.
-    if level not in (logging.DEBUG, logging.INFO, logging.WARNING,
-                     logging.ERROR, logging.CRITICAL):
-        level = getattr(logging, level.upper())
-
-    # TODO: looks like the following formatter is not used.
-    if timezone.lower() == 'UTC':
-        Formatter.converter = time.gmtime
-    elif timezone.lower() == 'local':
-        Formatter.converter = time.localtime
-    else:  # e.g. 'US/Pacific'
-        def custom_time(*args):  # pylint: disable=unused-argument
-            utc_dt = pytz.utc.localize(datetime.utcnow())
-            my_tz = pytz.timezone(timezone)
-            converted = utc_dt.astimezone(my_tz)
-            return converted.timetuple()
-        Formatter.converter = custom_time
-
-    datefmt = '%Y-%m-%d %H:%M:%S'
-
-    msg = '[%(asctime)s.%(msecs)02d ' + timezone + \
-        '; %(levelname)s; %(name)s, %(funcName)s, %(lineno)d]'
-
-    if with_process_name:
-        if with_thread_name:
-            fmt = f'{msg} [%(processName)s %(threadName)s]  %(message)s'
-        else:
-            fmt = f'{msg} [%(processName)s]  %(message)s'
+    if with_process_name and with_thread_name:
+        fmt = f'{msg};  %(processName)s %(threadName)s]  %(message)s'
+    elif with_process_name:
+        fmt = f'{msg};  %(processName)s]  %(message)s'
     elif with_thread_name:
-        fmt = f'{msg} [%(threadName)s]  %(message)s'
+        fmt = f'{msg};  %(threadName)s]  %(message)s'
     else:
-        fmt = f'{msg}  %(message)s'
+        fmt = f'{msg}]  %(message)s'
 
-    return dict(format=fmt, datefmt=datefmt, level=level, **kwargs)
+    return logging.Formatter(fmt=fmt, datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def config_logger(**kwargs) -> None:
-    kw = _make_config(**kwargs)
+def set_level(level: Union[str, int] = logging.INFO):
+    '''
+    In one application, call `set_level` on the top level once.
+    Do not set level anywhere else, and do not set level on any handler.
+    '''
+    if isinstance(level, str):
+        level = getattr(logging, level.upper())
+    level0 = rootlogger.level
+    rootlogger.setLevel(level)
+    return level0
 
-    rootlogger = logging.getLogger()
-    if rootlogger.hasHandlers():
-        rootlogger.handlers = []
 
-    logging.basicConfig(**kw)
+def use_console_handler(**kwargs):
+    if any(getattr(h, '_name_', None) == 'console' for h in rootlogger.handlers):
+        raise RuntimeError('the console handler is already in being used')
+    h = logging.StreamHandler()
+    h.setFormatter(formatter(**kwargs))
+    h._name_ = 'console'
+    rootlogger.addHandler(h)
 
-    logging.captureWarnings(True)
-    warnings.filterwarnings('default', category=ResourceWarning)
-    warnings.filterwarnings('default', category=DeprecationWarning)
-    warnings.filterwarnings(
-        'ignore', category=DeprecationWarning, module='ptpython')
-    warnings.filterwarnings(
-        'ignore', category=DeprecationWarning, module='jedi')
 
-    try:
-        # Turn off annoyance in ptpython when setting DEBUG logging
-        logging.getLogger('parso').setLevel(logging.ERROR)
-    except Exception:
-        pass
+def _unuse_handler(name: str):
+    for h in rootlogger.handlers:
+        if getattr(h, '_name_', None) == name:
+            rootlogger.removeHandler(h)
+            return 1
+    return 0
 
-    # This is how to turn on asyncio debugging.
-    # Don't turn it on in this function.
-    # asyncio.get_event_loop().set_debug(True)
+
+def unuse_console_handler():
+    return _unuse_handler('console')
+
+
+def use_disk_handler(*,
+                     foldername: str = None,
+                     maxBytes=1_000_000, backupCount=20, delay=True, **kwargs):
+    if any(getattr(h, '_name_', None) == 'disk' for h in rootlogger.handlers):
+        raise RuntimeError('the disk handler is already in being used')
+
+    if foldername:
+        foldername = foldername.rstrip('/')
+    else:
+        launcher = inspect.stack()[1].filename
+        foldername == f"{os.environ.get('LOGDIR', '/tmp/log')}/{launcher.lstrip('/').replace('/', '-')}"
+    print(f"Log files are located in '{foldername}'")
+    os.makedirs(foldername, exist_ok=True)
+    h = logging.handlers.RotatingFileHandler(
+        filename=foldername + '/current',
+        maxBytes=maxBytes, backupCount=backupCount, delay=delay)
+    h.setFormatter(formatter(**kwargs))
+    h._name_ = 'disk'
+    rootlogger.addHandler(h)
+    return foldername
+
+
+def unuse_disk_handler():
+    return _unuse_handler('disk')
+
+
+def log_uncaught_exception(handlers=None, logger=logger)
+    # locally bind `handlers` and `logger` in case the global references are gone
+    # when the exception handler is invoked.
+    if handlers is None:
+        handlers = rootlogger.handlers
+
+    def handle_exception(exc_type, exc_val, exc_tb):
+        if not issubclass(exc_tb, KeyboardInterrupt):
+            if sys.version_info.minor < 11:
+                logging.currentframe = lambda: sys._getframe(1)
+            fn, lno, func, sinfo = logger.findCaller(stack_info=False, stacklevel=1)
+            record = logger.makeRecord(
+                logger.name, logging.CRITICAL, fn, lno,
+                msg=exc_val, args=(), exc_info=(exc_type, exc_val, exc_tb),
+                func=func, sinfo=sinfo,
+            )
+
+            for h in rootlogger.handlers:
+                if getattr(h, '_name_', None) != 'console':
+                    h.handle(record)
+
+        sys.__excepthook__(exc_type, exc_val, exc_tb)
+
+    sys.excepthook = handle_exception
+
+
+def config_logger(level=logging.INFO, **kwargs):
+    # For use in one-off scripts.
+    set_level(level)
+    use_console_handler(**kwargs)
